60a96dd58b552448dab75ba966a176314e97c3ae
Tim Groeneboom <tim@timgroeneboom.com>
2025-06-14 09:04:27 +0200

    - samplerate & buffer size

--- system_modules/napportaudio/src/audio/service/portaudioservice.h
+++ system_modules/napportaudio/src/audio/service/portaudioservice.h
@@ -87,14 +87,14 @@ namespace nap
                 /**
                  * The buffer size the audio stream will run on, every audio callback processes this amount of samples per channel
                  */
-                int mBufferSize = 64;
+                int mBufferSize = 128;
 
                 /**
                  * The buffer size that is used internally by the node system to peform processing.
                  * This can be lower than mBufferSize but has to fit within mBufferSize a discrete amount of times.
                  * Lowering this can improve timing precision in the case that the node manager performs internal event scheduling, however will increase performance load.
                  */
-                int mInternalBufferSize = 64;
+                int mInternalBufferSize = 128;
             };
 
             /**
